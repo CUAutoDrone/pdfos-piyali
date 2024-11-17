63acb221b416c73ac02409003beec6fd39b328ac
Piyali Mittal <pm633@cornell.edu>
2024-11-17 13:07:46 -0500

    small tweaks

--- payload_function.py
+++ payload_function.py
@@ -21,7 +21,7 @@ def extract_parameters_with_value(value, file_path='output.json', target_value="
             if any(param.get("value") == target_value for param in parameters):
                 # Modify 'lengthValue' parameter within parameters array
                 for param in parameters:
-                    if param.get("parameterId") == "lengthValue":
+                    if param.get("parameterId") == "lengthValue" or param.get("parameterId") == "value" :
                         param["expression"] = f"{value} mm"
                 
                 # Append the modified parameters array to matching_parameters
@@ -37,8 +37,8 @@ def extract_parameters_with_value(value, file_path='output.json', target_value="
         return None
 
 # Example usage
-parameters_with_c1 = extract_parameters_with_value('output.json', target_value="C1")
-print(parameters_with_c1)  # Prints the extracted parameters containing "value": "C1"
+parameters_with_c1 = extract_parameters_with_value(file_path='output.json', target_value="C1", value=3)
+print(parameters_with_c1)  # Prints the extracted parameters containing "value": "C1" for value 3
 
 def return_payload_feature1_dict(id, value):
     payload1 = {"btType": "BTFeatureDefinitionCall-1406",
@@ -46,7 +46,7 @@ def return_payload_feature1_dict(id, value):
           "featureId": id,
           "featureType": "assignVariable",
           "name" : "###name = #value",
-          "parameters" : extract_parameters_with_value('output.json', target_value="C1", value=value)}}
+          "parameters" : extract_parameters_with_value(file_path='output.json', target_value="C1", value=value)}}
     
     return payload1
 
@@ -56,7 +56,7 @@ def return_payload_feature2_dict(id, value):
           "featureId": id,
           "featureType": "assignVariable",
           "name" : "###name = #value",
-          "parameters" : extract_parameters_with_value('output.json', target_value="C2", value=value)}}
+          "parameters" : extract_parameters_with_value(file_path='output.json', target_value="C2", value=value)}}
     
     return payload2
 
@@ -66,7 +66,7 @@ def return_payload_feature3_dict(id, value):
           "featureId": id,
           "featureType": "assignVariable",
           "name" : "###name = #value",
-          "parameters" : extract_parameters_with_value('output.json', target_value="C3", value=value)}}
+          "parameters" : extract_parameters_with_value(file_path='output.json', target_value="C3", value=value)}}
     
     return payload3
 
@@ -76,6 +76,6 @@ def return_payload_feature4_dict(id, value):
           "featureId": id,
           "featureType": "assignVariable",
           "name" : "###name = #value",
-          "parameters" : extract_parameters_with_value('output.json', target_value="C4", value=value)}}
+          "parameters" : extract_parameters_with_value(file_path='output.json', target_value="C4", value=value)}}
     
     return payload4
